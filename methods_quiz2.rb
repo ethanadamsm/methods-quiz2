8daaf881e92479c64cae66e1538abef5cbd7db14
Ethan Adams <ethanadamsm@s.dcsdk12.org>
2015-09-11 09:57:32 -0600

    max_maybe completed

--- methods_quiz2.rb
+++ methods_quiz2.rb
@@ -1,7 +1,7 @@
 module MethodsQuiz2
 	
 	def without_doubles(die_one, die_two)
-		if dice_equal?(die_one, die_two) 
+		if is_equal?(die_one, die_two) 
 			if is_die_six?(die_one)
 				7
 			else
@@ -12,8 +12,28 @@ module MethodsQuiz2
 		end
 	end
 
-	def dice_equal?(die_one, die_two)
-		die_one == die_two 
+	def max_maybe(a, b)
+		if is_equal?(a, b)
+			0 
+		else
+			if divisable_5?(a) && divisable_5?(b)
+				smaller?(a, b) ? a : b
+			else
+				smaller?(a, b) ? b : a 
+			end
+		end
+	end
+
+	def smaller?(a, b)
+		a < b 
+	end
+
+	def divisable_5?(x)
+		x % 5 == 0
+	end
+
+	def is_equal?(a, b)
+		a == b
 	end
 
 	def is_die_six?(die)
